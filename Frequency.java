fed680fd005b12ac65b7ce2195282e5be2aaea0b
ansaannandrews <ansaannandrews2027@cs.sjcetpalai.ac.in>
2024-09-12 12:03:32 +0530

    Add files via upload

---
+++ Frequency.java
@@ -0,0 +1,22 @@
+package oop;
+import java.util.Scanner;
+public class Frequency
+{
+    public static void main(String[] args)
+    {   
+    	Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str=sc.nextLine();
+		System.out.println("Enter the Character:");
+		char ch=sc.nextLine().charAt(0);
+		int count=0;
+		for(int i=0;i<(str.length());i++)
+		{
+			if(str.charAt(i)==ch)
+			{
+				count++;
+			}
+        }
+		System.out.println("Character "+ch+" occured "+count+" times");
+    }
+}
